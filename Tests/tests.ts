5161f58a09f15de9232268574ca9d6c8246ef6de
Shawn <shawn@ievolved.io>
2020-05-24 18:54:50 -0700

    Fix: Corrected the incorrect unit tests.

--- Tests/tests.ts
+++ Tests/tests.ts
@@ -44,8 +44,8 @@ Deno.test("sanitize", async () => {
 
   assertEquals(VerbalExpression.sanitize(testString), escaped, "Special characters should be sanitized");
 
-  equal(VerbalExpression.sanitize(42), 42);
-  equal(VerbalExpression.sanitize(/foo/), "foo");
+  assertEquals(VerbalExpression.sanitize(42), "42");
+  assertEquals(VerbalExpression.sanitize(/foo/), "foo");
 });
 
 Deno.test("add", async () => {
@@ -65,53 +65,53 @@ Deno.test("startOfLine", async () => {
   let testRegex = VerEx().startOfLine().then("a");
   let testString = 'a';
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = 'ba';
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = testRegex.startOfLine(false); // start of line is no longer necessary
   testString = "ba";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 });
 
 Deno.test("endOfLine", async () => {
   let testRegex = VerEx().find("a").endOfLine();
   let testString = "a";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "ab";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = testRegex.endOfLine(false); // end of line is no longer necessary
   testString = "ab";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 });
 
 function then(name: string) {
   let testRegex = VerEx()[name]("a");
   let testString = "a";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "b";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx()[name]("a")[name]("b");
   testString = "ab";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "ac";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 }
 
 Deno.test("then", async () => {
@@ -127,45 +127,45 @@ Deno.test("maybe", async () => {
   const testRegex = VerEx().startOfLine().then("a").maybe("b");
   let testString = "acb";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "abc";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 });
 
 Deno.test("or", async () => {
   let testRegex = VerEx().startOfLine().then("abc").or("def");
   let testString = "defzzz";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "abczzz";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "xyzabc";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx().startOfLine().then("abc").or().then("def");
   testString = "defzzz";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "abczzz";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "xyzabc";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 });
 
 Deno.test("anything", async () => {
   const testRegex = VerEx().startOfLine().anything();
   let testString = "foo";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "";
@@ -176,11 +176,11 @@ Deno.test("anythingBut", async () => {
   let testRegex = VerEx().startOfLine().anythingBut("br").endOfLine();
   let testString = "foobar";
 
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "foo_a_";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   testRegex = VerEx().startOfLine().anythingBut("br");
   testString = "bar";
@@ -188,11 +188,11 @@ Deno.test("anythingBut", async () => {
 
   testRegex = VerEx().startOfLine().anythingBut(["b", "r"]).endOfLine();
   testString = "foobar";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "foo_a_";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   testRegex = VerEx().startOfLine().anythingBut(["b", "r"]);
   testString = "bar";
@@ -203,65 +203,66 @@ Deno.test("something", async () => {
   const testRegex = VerEx().something();
   let testString = "";
 
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "a";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 });
 
 Deno.test("somethingBut", async () => {
   let testRegex = VerEx().startOfLine().somethingBut("abc").endOfLine();
   let testString = "";
-  equal(testRegex.test(testString), false);
+
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "foo";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "fab";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx().startOfLine().somethingBut(["a", "b", "c"]).endOfLine();
   testString = "";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "foo";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "fab";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 });
 
 function anyOf(name : string) {
   let testRegex = VerEx().startOfLine().then("a")[name]('xyz');
   let testString = 'ay';
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "ab";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "a";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx().startOfLine().then("a")[name](["x", "y", "z"]);
   testString = "ay";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "ab";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "a";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 }
 
 Deno.test("anyOf", async () => {
@@ -276,26 +277,26 @@ Deno.test("not", async () => {
   const testRegex = VerEx().startOfLine().not("foo").anything().endOfLine();
   let testString = "foobar";
 
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "bar";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 });
 
 Deno.test("range", async () => {
   let testRegex = VerEx().startOfLine().range("a", "z", "0", "9").oneOrMore().endOfLine();
   let testString = "foobarbaz123";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "fooBarBaz_123";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx().startOfLine().range("a", "z", "0").oneOrMore().endOfLine();
   testString = "foobarbaz";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "foobarbaz123";
@@ -309,19 +310,19 @@ function lineBreak(name: string) {
   const testRegex = VerEx().startOfLine().then("abc")[name]().then("def");
   let testString = "abc\r\ndef";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "abc\ndef";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "abc\rdef";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "abc\r\n\ndef";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 }
 
 Deno.test("lineBreak", async () => {
@@ -336,44 +337,44 @@ Deno.test("tab", async () => {
   const testRegex = VerEx().startOfLine().tab().then("abc");
   let testString = "\tabc";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "abc";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 });
 
 Deno.test("word", async () => {
   let testRegex = VerEx().startOfLine().word().endOfLine();
   let testString = "azertyuiopqsdfghjklmwxcvbn0123456789_";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   testRegex = VerEx().word();
   testString = ". @[]|,&~-";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 });
 
 Deno.test("digit", async () => {
   let testRegex = VerEx().startOfLine().digit().oneOrMore().endOfLine();
   let testString = "0123456789";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   testRegex = VerEx().digit();
   testString = "-.azertyuiopqsdfghjklmwxcvbn @[]|,_&~";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 });
 
 Deno.test("whitespace", async () => {
   const testRegex = VerEx().startOfLine().whitespace().oneOrMore().searchOneLine().endOfLine();
   let testString = " \t\r\n\v\f";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "a z";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 });
 
 
@@ -381,7 +382,7 @@ Deno.test("whitespace", async () => {
 
 Deno.test("addModifier", async () => {
   let testRegex = VerEx().addModifier("y");
-  equal(testRegex.flags.includes("y"), true);
+  assertEquals(testRegex.flags.includes("y"), true);
 
   //t.notThrows(() => {
   //  testRegex = VerEx().addModifier('g');
@@ -390,26 +391,26 @@ Deno.test("addModifier", async () => {
 
 Deno.test("removeModifier", async () => {
   const testRegex = VerEx().removeModifier("g");
-  equal(testRegex.flags.includes("g"), false);
+  assertEquals(testRegex.flags.includes("g"), false);
 });
 
 Deno.test("withAnyCase", async () => {
   let testRegex = VerEx().startOfLine().then("a");
   let testString = "A";
 
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx().startOfLine().then("a").withAnyCase();
   testString = "A";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "a";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   testRegex = VerEx().startOfLine().then("a").withAnyCase(false);
   testString = "A";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 });
 
 Deno.test("stopAtFirst", async () => {
@@ -434,13 +435,13 @@ Deno.test("searchOneLine", async () => {
   let testRegex = VerEx().startOfLine().then("b").endOfLine();
   const testString = "a\nb\nc";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   testRegex = VerEx().startOfLine().then("b").endOfLine().searchOneLine();
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx().startOfLine().then("b").endOfLine().searchOneLine(false);
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 });
 
 
@@ -450,19 +451,19 @@ Deno.test("repeatPrevious", async () => {
   let testRegex = VerEx().startOfLine().find("foo").repeatPrevious(3).endOfLine();
   let testString = "foofoofoo";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "foofoo";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "foofoofoofoo";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "bar";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx().startOfLine().find("foo").repeatPrevious(1, 3).endOfLine();
 
@@ -471,9 +472,9 @@ Deno.test("repeatPrevious", async () => {
     testString = "foo".repeat(i);
 
     if (i < 1 || i > 3) {
-      equal(testRegex.test(testString), false);
+      assertEquals(testRegex.test(testString), false);
     } else {
-      equal(testRegex.test(testString), true);
+      assertEquals(testRegex.test(testString), true);
     }
   }
 
@@ -490,50 +491,50 @@ Deno.test("oneOrMore", async () => {
   const testRegex = VerEx().startOfLine().then("foo").oneOrMore().endOfLine();
   let testString = "foo";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "foofoo";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "bar";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 });
 
 Deno.test("multiple", async () => {
   let testRegex = VerEx().startOfLine().find(" ").multiple().endOfLine();
   let testString = "   ";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = " a ";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   testRegex = VerEx().startOfLine().multiple("foo").endOfLine();
   testString = "foo";
 
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "foofoofoo";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   testRegex = VerEx().startOfLine().multiple("foo", 2).endOfLine();
   testString = "foo";
-  equal(testRegex.test(testString), false);
+  assertEquals(testRegex.test(testString), false);
 
   resetLastIndex(testRegex);
   testString = "foofoo";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   testString = "foofoofoo";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   testRegex = VerEx().startOfLine().multiple("foo", 2, 5).endOfLine();
 
@@ -542,9 +543,9 @@ Deno.test("multiple", async () => {
     testString = "foo".repeat(i);
 
     if (i < 2 || i > 5) {
-      equal(testRegex.test(testString), false);
+      assertEquals(testRegex.test(testString), false);
     } else {
-      equal(testRegex.test(testString), true);
+      assertEquals(testRegex.test(testString), true);
     }
   }
 });
@@ -560,7 +561,7 @@ Deno.test("capture groups", async () => {
 
   testRegex = testRegex.endCapture().then("baz");
   testString = "foobarbaz";
-  equal(testRegex.test(testString), true);
+  assertEquals(testRegex.test(testString), true);
 
   resetLastIndex(testRegex);
   const matches = testRegex.exec(testString);
